6696bc5da494a7a9bc357639306fd22c46f5c7c2
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2022-08-31 22:55:42 +0500

    обновит test1.js

--- test1.js
+++ test1.js
@@ -8,3 +8,18 @@ divElement.style.width = `200px`;
 divElement.style.height = `200px`;
 divElement.style.background = `blue`;
 document.body.appendChild(divElement);
+
+// fetch('http://translate.sitotest.ru/translate?q=test')
+//     .then((res) => res.json())
+//     .then((data) => {console.log(data);})
+//     .catch((err) => console.log(err));
+
+fetch('https://fat.sitotest.ru/data.json')
+    .then((res) => res.json())
+    .then((data) => {console.log(data);})
+    .catch((err) => console.log(err));
+
+// 1. Развернуть в докере проект translate
+// 2. Прописать переадресацию в настройках nginx
+// 3. Создать поддомен translate.sitotest.ru для 95.78.239.48
+// 4. Запросить сертификат для translate.sitotest.ru
